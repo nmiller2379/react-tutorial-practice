e683d801c77218457805932e103e5dfa12ce3cfc
Miller <nmiller2379@gmail.com>
2024-06-16 13:52:02 -0400

    tweaked CSS a bit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,5 @@ import React from "react";
 import Game from "./components/Game/Game";
 
 export default function App() {
-  return (
-    <div>
-      <Game />
-    </div>
-  );
+  return <Game />;
 }
